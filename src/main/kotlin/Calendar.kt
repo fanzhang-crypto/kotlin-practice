419cfdecf8434c26738d2837ca740e7ca0a9adb0
fanzhang-crypto <fan.zhang@crypto.com>
2023-02-21 18:41:43 +0800

    print year title

--- src/main/kotlin/Calendar.kt
+++ src/main/kotlin/Calendar.kt
@@ -1,6 +1,6 @@
-import java.lang.StringBuilder
 import java.time.DayOfWeek.*
 import java.time.LocalDate
+import java.time.Month
 import java.time.Year
 import java.time.format.TextStyle
 import java.time.temporal.WeekFields
@@ -27,7 +27,6 @@ import java.util.*
  *  17 18 19 20 21 22 23  22 23 24 25 26 27 28  19 20 21 22 23 24 25
  *  24 25 26 27 28 29 30  29 30 31              26 27 28 29 30
  *
- *
  *         July                 August               September
  *  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
  *                  1  2      1  2  3  4  5  6               1  2  3
@@ -47,92 +46,106 @@ import java.util.*
  *  30 31
  */
 fun main() {
-    Calendar(Year.of(2022), Year.of(2023), Locale.US)
-        .render(Calendar.Layout(3))
-        .let { println(it) }
+    Calendar(Year.of(2022), Year.of(2024), Calendar.Layout(columns = 3))
+        .render()
+        .forEach(::println)
 }
 
 class Calendar(
     private val yearStart: Year,
     private val yearEnd: Year,
+    private val layout: Layout,
     private val locale: Locale = Locale.US
 ) {
     data class Layout(
         val columns: Int,
+        val cellWidth: Int = 3,
         val rowGap: Int = 1,
         val columnGap: Int = 1,
     )
 
-    fun render(layout: Layout): String = datesBetween(yearStart, yearEnd)
-        .byMonth()
-        .map { month -> layoutMonth(month) }
-        .chunked(layout.columns) { monthLayouts -> combineMonthLayouts(monthLayouts, layout) }
-        .map { it.joinToString("\n") }
-        .joinToString("\n")
+    private val emptyCell = " ".repeat(layout.cellWidth)
+    private val emptyWeek = emptyCell.repeat(WEEK_DAYS.size)
+
+    fun render(): Sequence<String> = datesBetween(yearStart, yearEnd)
+        .byYear()
+        .flatMap { year ->
+            year.byMonth()
+                .map { month -> layoutMonth(month) }
+                .chunked(layout.columns, ::joinMonths)
+                .flatMap { it }
+        }
 
     private fun layoutMonth(datesOfMonth: Sequence<LocalDate>): Sequence<String> = sequence {
         val weekDaysTitle = WEEK_DAYS.joinToString(separator = " ", prefix = " ") {
-            it.getDisplayName(TextStyle.SHORT, locale).truncate(CELL_WITH - 1)
+            it.getDisplayName(TextStyle.SHORT, locale).fit(layout.cellWidth - 1)
         }
 
-        val month = datesOfMonth.first().month
-        val monthTitle = month.getDisplayName(TextStyle.FULL, locale).center(weekDaysTitle.length)
+        val firstDate = datesOfMonth.first()
+        val month = firstDate.month
 
+        val monthTitle = month.getDisplayName(TextStyle.FULL, locale).center(emptyWeek.length)
+
+        val yearTitle =
+            if (month == Month.JANUARY)
+                firstDate.year.toString().left(emptyWeek.length)
+            else emptyWeek
+
+        yield(yearTitle)
         yield(monthTitle)
         yield(weekDaysTitle)
 
-        val weeks = datesOfMonth.byWeek().map { week ->
-            val firstDayOfWeek = week.first().dayOfWeek
-            val lastDayOfWeek = week.last().dayOfWeek
+        val emptyWeekLines = generateSequence { emptyWeek }
 
-            val leftIndent = EMPTY_CELL.repeat(WEEK_DAYS.indexOf(firstDayOfWeek))
-            val rightIndent = EMPTY_CELL.repeat(WEEK_DAYS.reversed().indexOf(lastDayOfWeek))
-            val datesString = week.map { date -> String.format("%${CELL_WITH}d", date.dayOfMonth) }.joinToString("")
-
-            StringBuilder(weekDaysTitle.length)
-                .append(leftIndent)
-                .append(datesString)
-                .append(rightIndent)
-                .toString()
-        }
-
-        yieldAll(weeks)
+        (datesOfMonth.byWeek().map { layoutWeek(it) } + emptyWeekLines)
+            .take(WEEK_LINES_PER_MONTH)
+            .forEach { weekLine -> yield(weekLine) }
     }
 
-    private fun combineMonthLayouts(monthsOfSameRow: List<Sequence<String>>, layout: Layout): Sequence<String> {
-        val columnGap = " ".repeat(layout.columnGap)
-        val row = monthsOfSameRow.reduce { acc, month ->
-            acc.zipWithDefault(
-                month,
-                EMPTY_WEEK,
-                EMPTY_WEEK
-            ) { a, b -> "$a$columnGap$b" }
-        }
-        val rowGap = Array(layout.rowGap) { " " }.asSequence()
+    private fun layoutWeek(week: Sequence<LocalDate>): String {
+        val firstDayOfWeek = week.first().dayOfWeek
+        val lastDayOfWeek = week.last().dayOfWeek
 
+        val leftIndent = emptyCell.repeat(WEEK_DAYS.indexOf(firstDayOfWeek))
+        val rightIndent = emptyCell.repeat(WEEK_DAYS.reversed().indexOf(lastDayOfWeek))
+        val datesString = week.joinToString("") { date -> date.dayOfMonth.toString().fit(layout.cellWidth) }
+
+        return leftIndent + datesString + rightIndent
+    }
+
+    private fun joinMonths(months: List<Sequence<String>>): Sequence<String> {
+        val columnGap = " ".repeat(layout.columnGap)
+        val row = months
+            .reduce { acc, month ->
+                acc.zip(month) { a, b -> "$a$columnGap$b" }
+            }
+            .filter { it.isNotBlank() }
+
+        val rowGap = Array(layout.rowGap) { " " }.asSequence()
         return row + rowGap
     }
 
     companion object {
         private val WEEK_DAYS = arrayOf(SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY)
 
-        private const val CELL_WITH = 3
+        private const val WEEK_LINES_PER_MONTH = 6
 
-        private val EMPTY_CELL = " ".repeat(CELL_WITH)
-        private val EMPTY_WEEK = EMPTY_CELL.repeat(WEEK_DAYS.size)
-
-        private fun datesBetween(yearStart: Year, yearEnd: Year): Sequence<LocalDate> = sequence {
+        fun datesBetween(yearStart: Year, yearEnd: Year): Sequence<LocalDate> = sequence {
             var currentDate = yearStart.atDay(1)
             while (currentDate.year < yearEnd.value) {
                 yield(currentDate)
+//                println("yield $currentDate")
                 currentDate = currentDate.plusDays(1)
             }
         }
 
-        private fun Sequence<LocalDate>.byMonth(): Sequence<Sequence<LocalDate>> =
+        fun Sequence<LocalDate>.byYear(): Sequence<Sequence<LocalDate>> =
+            this.groupByConsecutiveLazily { d1, d2 -> d1.year == d2.year }
+
+        fun Sequence<LocalDate>.byMonth(): Sequence<Sequence<LocalDate>> =
             this.groupByConsecutive { d1, d2 -> d1.year == d2.year && d1.month == d2.month }
 
-        private fun Sequence<LocalDate>.byWeek(): Sequence<Sequence<LocalDate>> =
+        fun Sequence<LocalDate>.byWeek(): Sequence<Sequence<LocalDate>> =
             this.groupByConsecutive { d1, d2 -> d1.year == d2.year && d1.month == d2.month && d1.weekOfMonth == d2.weekOfMonth }
 
         private fun <T> Sequence<T>.groupByConsecutive(predicate: (T, T) -> Boolean): Sequence<Sequence<T>> {
@@ -141,11 +154,7 @@ class Calendar(
             return sequence {
                 var group = mutableListOf<T>()
                 for (item in upstream) {
-                    if (group.isEmpty()) {
-                        group += item
-                        continue
-                    }
-                    if (predicate(group.last(), item)) {
+                    if (group.isEmpty() || predicate(group.last(), item)) {
                         group += item
                         continue
                     }
@@ -158,41 +167,54 @@ class Calendar(
             }
         }
 
+        private fun <T> Sequence<T>.groupByConsecutiveLazily(predicate: (T, T) -> Boolean): Sequence<Sequence<T>> {
+            val upstream = this.iterator()
+
+            return sequence {
+                var lastItem: T? = null
+
+                while (upstream.hasNext()) {
+                    val group = sequence {
+                        if (lastItem != null) {
+                            yield(lastItem!!)
+                        }
+                        for (item in upstream) {
+                            if (lastItem == null || predicate(lastItem!!, item)) {
+                                yield(item)
+                                lastItem = item
+                            } else {
+                                lastItem = item
+                                break
+                            }
+                        }
+                    }
+                    yield(group)
+                }
+            }
+        }
+
         private val LocalDate.weekOfYear: Int
             get() = get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear())
 
         private val LocalDate.weekOfMonth: Int
             get() = get(WeekFields.of(Locale.getDefault()).weekOfMonth())
 
-        private fun <L, R, V> Sequence<L>.zipWithDefault(
-            other: Sequence<R>,
-            leftDefault: L,
-            rightDefault: R,
-            transform: (a: L, b: R) -> V
-        ): Sequence<V> = sequence {
-            val leftIter = iterator()
-            val rightIter = other.iterator()
-
-            while (leftIter.hasNext() && rightIter.hasNext()) {
-                yield(transform(leftIter.next(), rightIter.next()))
-            }
-            while (leftIter.hasNext()) {
-                yield(transform(leftIter.next(), rightDefault))
-            }
-            while (rightIter.hasNext()) {
-                yield(transform(leftDefault, rightIter.next()))
-            }
+        private fun String.fit(width: Int): String = when {
+            length >= width -> truncate(width)
+            else -> padStart(width)
         }
 
-        private fun String.center(size: Int, pad: String = " "): String {
-            if (this.length >= size) {
+        private fun String.center(width: Int): String {
+            if (length >= width) {
                 return this
             }
-            val leftPad = pad.repeat((size - this.length) / 2)
-            val rightPad = pad.repeat(size - this.length - leftPad.length)
+            val leftPadLength = (width - length) / 2
 
-            return "$leftPad$this$rightPad"
+            return this.padStart(leftPadLength + length).padEnd(width)
         }
+
+        private fun String.left(width: Int) = this.padEnd(width)
+        private fun String.right(width: Int) = this.padStart(width)
 
         private fun String.truncate(size: Int): String =
             if (size >= length) this else substring(0 until size)
